7c5f23cfcde8ad20cb1f692a276b71697cdd5576
DeSireFire <1025212779@qq.com>
2016-07-10 00:01:33 +0800

    添HTMl

---
+++ Django/day72(html转义)/demo/webapp/user/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import include, url
+from user.views import *
+
+
+urlpatterns=[
+    url(r'^register$',register,name='register'),
+    url(r'^register_handler$',register_handler,name='register_handler'),
+    url(r'^login$', login, name='login'),
+    url(r'^login_handler$', login_handler, name='login_handler'),
+    url(r'^test1$', test1),
+]
---
+++ Django/day72(html转义)/demo/webapp/user/views.py
@@ -0,0 +1,70 @@
+from django.shortcuts import render,redirect
+from user.models import  *
+from hashlib import *
+from django.http import *
+from django.template import loader,RequestContext
+from django.core.urlresolvers import reverse
+from datetime import timedelta
+
+# Create your views here.
+def register(request):
+    return render(request, 'user/register.html')
+
+
+def register_handler(request):
+    user = User()
+    user.uname = request.POST.get('username')
+    user.upwd = sha1(request.POST.get('userpwd').encode('utf-8')).hexdigest()
+    user.save()
+    return render(request, 'user/success.html')
+
+
+def login(request):
+    print('session:%s'%request.session.get('currentUser'))
+
+    context = {}
+    #获取cookie
+    username = request.COOKIES.get('mycooki')
+    if username:
+        context['username']=username
+    return render(request, 'user/login.html',context)
+
+def login_handler(request):
+    #定义上下文
+    context={}
+    #用户名密码
+    username = request.POST.get('username')
+    userpwd = sha1(request.POST.get('userpwd').encode('utf-8')).hexdigest()
+
+    #匹配
+    ret = User.objects.filter(uname=username,upwd=userpwd)
+    if len(ret)==0:
+        return HttpResponseRedirect('/user/login')
+    else:
+        #在服务端保持一个session键值对
+        request.session['currentUser'] = username
+        request.session.set_expiry(36000)
+        #request.session.set_expiry(timedelta(days=2))
+
+        #加载模板
+        t1 = loader.get_template('user/success.html')
+        #上下文
+        requestcontext = RequestContext(request,context)
+        #创建具有模板和上下文的reponse
+        response = HttpResponse(t1.render(requestcontext))
+        #记录用户名密码的变量
+        rememberName = request.POST.get('rememberName')
+        #判断
+        if rememberName=='1':
+            #写cookie
+            response.set_cookie('mycookie',username,max_age=3600)
+        return response
+
+
+
+
+
+def test1(request):
+    context = {'name':'<h1>laowang</h1>'}
+    return render(request, 'user/test1.html',context)
+    #return HttpResponse('<h1>laowang</h1>')
